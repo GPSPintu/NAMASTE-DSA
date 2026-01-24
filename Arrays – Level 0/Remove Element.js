4ede7a0b4f2ee8ae1b7fe0e2f4eb4b49e5d30b67
GPSPintu <pintugps.in@gmail.com>
2026-01-24 22:16:00 +0530

    Remove Elements

--- Arrays – Level 0/Remove Element.js
+++ Arrays – Level 0/Remove Element.js
@@ -5,25 +5,24 @@
 // Return the number of elements that are NOT equal to val.
 
 var removeElement = function (nums, val) {
-    let x = 0; // Pointer to place the next valid (non-val) element
+    let x = 0; // Pointer to track the position for the next valid element
 
     for (let i = 0; i < nums.length; i++) {
-        // If current element is NOT equal to val,
-        // copy it to index x and move x forward
+        // If the current element is NOT equal to val,
+        // place it at index x and move x forward
         if (nums[i] !== val) {
             nums[x] = nums[i];
             x++;
-        }   
-    // x represents the count of elements not equal to val
-    return x;
+        }
     }
 
+    // x represents the count of elements not equal to val
+    return x;
 };
 
 // Example input
 let result = [3, 2, 1, 5, 3, 4, 8, 3];
 let k = removeElement(result, 3);
 
-// Output
 console.log(`The number of removed elements is ${result.length - k}`);
 console.log(`The new array is: ${result.slice(0, k)}`);
