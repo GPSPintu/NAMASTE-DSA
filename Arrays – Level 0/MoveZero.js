873999b57d8a316a0beeba560832ffd57ce3b48b
GPSPintu <pintugps.in@gmail.com>
2026-02-17 08:21:09 +0530

    Move Zero

--- Arrays – Level 0/MoveZero.js
+++ Arrays – Level 0/MoveZero.js
@@ -1,23 +1,33 @@
-// Given an integer array nums, move all 0s to the end while maintaining the relative order of the non-zero elements.
-
-//  Conditions:
-// Do it in-place.
-// Do NOT create a new array.
+// Problem:
+// Given an integer array nums, move all 0s to the end
+// while maintaining the relative order of non-zero elements.
+//
+// Conditions:
+// 1. Modify the array in-place.
+// 2. Do NOT create a new array.
 
 var moveZeroes = function(nums) {
 
-    // Pointer for placing non-zero elements
+    // Pointer 'x' tracks the position where next non-zero element should be placed
     let x = 0;
 
-    // Move non-zero elements to front
+    // First loop:
+    // Move all non-zero elements to the front of the array
     for (let i = 0; i < nums.length; i++) {
+
+        // If current element is not zero
         if (nums[i] !== 0) {
+
+            // Place it at index 'x'
             nums[x] = nums[i];
+
+            // Move pointer forward
             x++;
         }
     }
 
-    // Fill remaining positions with zeros
+    // Second loop:
+    // Fill remaining positions (after last non-zero) with zeros
     for (let i = x; i < nums.length; i++) {
         nums[i] = 0;
     }
